bd44ccec4553c56d626e74d2e7ed65ff6a0e28d6
Anderson Souza <andersom_jag@hotmail.com>
2017-09-13 20:42:26 -0300

    removendo view padrao

--- src/routes.php
+++ src/routes.php
@@ -22,7 +22,18 @@ $app->get('/teste', function ($request, $response) {
     $battleDao->adicionarBatalha(new Battle("Chitãozinho e chororó - Evidencias","Rafaga - Mentirosa"));
 
 });
+$app->get('/admin/battle/{battle}/encerrar', function ($request, $response, $args) {
+    $battle = new Battle();
+    $battle->setId($args['battle']);
+    (new BattleDao())->encerrarBatalha($battle);
 
+});
+$app->get('/admin/battle/{battle}/deletar', function ($request, $response) {
+
+});
+$app->post('/admin/battle/{battle}', function ($request, $response) {
+
+});
 
 $app->post('/{musica:[1,2]}/votar', "Ander\Controllers\VotoController:votar");
 $app->post('/', "Ander\Controllers\ApiController:all");
