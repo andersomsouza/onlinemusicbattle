00b34ad962f608b1e69fbebfbc441d5b6ebddd72
Andersom Souza <andersom.eugenio@gmail.com>
2018-03-03 13:12:15 -0300

    null tests

--- src/Ander/Controllers/ApiController.php
+++ src/Ander/Controllers/ApiController.php
@@ -29,7 +29,7 @@ class ApiController
 
         $resposta = array();
         $resposta['musicas'] = array();
-        if (!is_null($battle)) {
+        if (!is_null($battle) && !is_null($battle->getMusica1()) && !is_null($battle->getMusica2()) ) {
             $porcentagemVoto1 = 0;
             $porcentagemVoto2 = 0;
             $totalDeVotos = $battle->getMusica1()->getVotos() + $battle->getMusica2()->getVotos();
@@ -37,8 +37,9 @@ class ApiController
             $porcentagemVoto2 = $totalDeVotos > 0 ? number_format(100 * $battle->getMusica2()->getVotos() / $totalDeVotos, 1) : 0;
             $resposta['musicas'][$battle->getMusica1()->getNome()] = "${porcentagemVoto1} %";
             $resposta['musicas'][$battle->getMusica2()->getNome()] = "${porcentagemVoto2} %";
-            $resposta['ultimaVitoria'] = (!empty($battleFinalizada)) ? $battleFinalizada->getMusicaVencedora() : "";
         }
+        $resposta['ultimaVitoria'] = (!empty($battleFinalizada)) ? $battleFinalizada->getMusicaVencedora() : "";
+
         if (isset($_SESSION['last_vote']) && $_SESSION['last_vote'] == $battle->getId()) {
             $resposta['ja_votou'] = true;
         } else {
